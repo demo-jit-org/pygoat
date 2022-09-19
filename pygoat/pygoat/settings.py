3457551726def4a4b92e3872a4b2ee05a04fa164
raz-shlomo-frontegg <110029056+raz-shlomo-frontegg@users.noreply.github.com>
2022-09-19 11:18:32 +0300

    Update settings.py

--- pygoat/pygoat/settings.py
+++ pygoat/pygoat/settings.py
@@ -21,8 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'lr66%-a!$km5ed@n5ug!tya5bv!0(yqwa1tn!q%0%3m2nh%oml'
+
 
 SENSITIVE_DATA = 'FLAGTHATNEEDSTOBEFOUND'
 
